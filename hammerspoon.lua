876baa9d6621a8a87069cde4b92f2d363e162fdc
Radovan Haluska <radovan.haluska1@gmail.com>
2021-12-31 22:00:49 +0100

    Add a shortcut for making an application fullscreen

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -9,3 +9,17 @@ end)
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
     hs.window.focusedWindow():centerOnScreen()
 end)
+
+-- Resize an active window to a fullscreen
+hs.hotkey.bind({'cmd', 'alt'}, 'f', function()
+    local window = hs.window.focusedWindow()
+    local windowFrame = window:frame()
+    local screenFrame = window:screen():frame()
+
+    windowFrame.x = 0
+    windowFrame.y = 0
+    windowFrame.w = screenFrame.w
+    windowFrame.h = screenFrame.h
+
+    window:setFrame(windowFrame)
+end)
