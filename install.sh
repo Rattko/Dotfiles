947bb01404929d526a1757beb830923a616bcbbe
Radovan Haluska <radovan.haluska1@gmail.com>
2022-09-14 16:22:22 +0200

    Replace `realpath` with a custom function

--- install.sh
+++ install.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env zsh
 
+# Helper function to retrieve an absolute path of a given file
+get_absolute_path() {
+    echo "$(find $(pwd) -maxdepth 1 -name "$1")"
+}
+
 # Create needed directories
 mkdir -p ~/.zsh ~/.config/{bat,git,kitty}
 
 # Kitty
-ln -s -f $(realpath kitty.conf) ~/.config/kitty/kitty.conf
+ln -s -f $(get_absolute_path kitty.conf) ~/.config/kitty/kitty.conf
 
 # Zsh
-ln -s -f $(realpath zshenv) ~/.zshenv
+ln -s -f $(get_absolute_path zshenv) ~/.zshenv
 
 for file in aliases exports functions settings zprofile zshrc; do
-    ln -s -f $(realpath $file) ~/.zsh/.$file
+    ln -s -f $(get_absolute_path $file) ~/.zsh/.$file
 done
 
 # Starship
-ln -s -f $(realpath starship.toml) ~/.config/starship.toml
+ln -s -f $(get_absolute_path starship.toml) ~/.config/starship.toml
 
 # Vim
-ln -s -f $(realpath vimrc) ~/.vimrc
+ln -s -f $(get_absolute_path vimrc) ~/.vimrc
 
 # Git
-ln -s -f $(realpath gitconfig) ~/.config/git/config
-ln -s -f $(realpath gitignore) ~/.config/git/ignore
+ln -s -f $(get_absolute_path gitconfig) ~/.config/git/config
+ln -s -f $(get_absolute_path gitignore) ~/.config/git/ignore
 
 # Bat
-ln -s -f $(realpath bat.conf) ~/.config/bat/config
+ln -s -f $(get_absolute_path bat.conf) ~/.config/bat/config
